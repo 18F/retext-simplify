12715b6e48c5d04b85ba80f42296ea867eef7123
Brian Hedberg <bshslowfood@gmail.com>
2017-03-04 02:31:55 -0600

    added case support, modularize

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ var nlcstToString = require('nlcst-to-string');
 var quotation = require('quotation');
 var search = require('nlcst-search');
 var position = require('unist-util-position');
-var merge = require('deepmerge')
-var patterns = merge.all(require('./rules.js').all)
+var merge = require('deepmerge');
+
+var rules = [
+  require('./rules/brands.json'),
+  require('./rules/forbidden.json'),
+  require('./rules/government-words.json'),
+  require('./rules/18f-style.json')
+];
+
+var patterns = merge.all(rules);
 
 module.exports = simplify;
 
@@ -15,19 +23,33 @@ var list = keys(patterns);
 
 function simplify(options) {
   var ignore = (options || {}).ignore || [];
+
   var phrases = difference(list, ignore);
+  // console.log(phrases)
 
   return transformer;
 
   function transformer(tree, file) {
+    // console.log(phrases)
     search(tree, phrases, finder);
 
+    function casedMatch(match, replace) {
+      console.log(match, replace)
+      console.log(replace.indexOf(match) > -1)
+      return replace.indexOf(match) > -1
+    }
+
     function finder(match, index, parent, phrase) {
+
       var pattern = patterns[phrase];
       var replace = pattern.replace;
-      var value = quotation(nlcstToString(match), '“', '”');
+      var astMatch = nlcstToString(match)
+      var value = quotation(astMatch, '“', '”');
       var reason;
       var message;
+
+      var cased = casedMatch(astMatch, replace)
+
 
       if (pattern.omit && replace.length === 0) {
         reason = 'Remove ' + value;
@@ -38,14 +60,22 @@ function simplify(options) {
           reason += ', or remove it';
         }
       }
+      // console.log('---------')
+      // console.log(pattern, pattern.cased, !cased, !pattern.omit)
+      // console.log(pattern.replace)
+      // console.log((pattern.cased && !cased) || !pattern.omit)
+      // console.log(reason)
+      // console.log('---------')
 
-      message = file.warn(reason, {
-        start: position.start(match[0]),
-        end: position.end(match[match.length - 1])
-      });
+      if ((pattern.cased && !cased) || pattern.omit || !pattern.cased) {
+        message = file.warn(reason, {
+          start: position.start(match[0]),
+          end: position.end(match[match.length - 1])
+        });
 
-      message.ruleId = phrase.replace(/\s+/g, '-').toLowerCase();
-      message.source = 'retext-simplify';
+        message.ruleId = phrase.replace(/\s+/g, '-').toLowerCase();
+        message.source = 'retext-simplify';
+      }
     }
   }
 }
--- rules.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  all: [
-    require('./rules/brands.json'),
-    require('./rules/forbidden.json'),
-    require('./rules/government-words.json'),
-    require('./rules/18f-style.json')
-  ]
-}
